937997fc77f45e0ac3c26c6e3fa820100992dd8d
muzi55 <muzi5@kakao.com>
2023-09-23 22:58:31 +0900

    여러장 이미지 추가;

--- src/app/components/Blob.tsx
+++ src/app/components/Blob.tsx
@@ -4,18 +4,20 @@ import React, { useState } from "react";
 const BlobImg = (): JSX.Element => {
   const [imgList, setImgList] = useState<string[]>();
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    console.log(file);
+    const file = e.target.files;
 
     if (file !== undefined) {
-      const blobURL = URL.createObjectURL(file);
-      setImgList([blobURL]);
+      const blobURL = [...file].map((el) => URL.createObjectURL(el));
+      console.log(blobURL);
+      setImgList(blobURL);
     }
   };
   return (
     <>
-      <input type="file" accept="image/jpeg, image/png, image/webp" onChange={onChange} />
-      <img src={imgList?.[0]} alt="123" />
+      <input type="file" multiple accept="image/jpeg, image/png, image/webp" onChange={onChange} />
+      {imgList?.map((el, index) => (
+        <img key={el + index} src={el} alt="이미지 미리보기" />
+      ))}
     </>
   );
 };
