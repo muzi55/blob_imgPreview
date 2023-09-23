b036b3a531e8d7db6eeaf51bc674ef652f8c33aa
muzi55 <muzi5@kakao.com>
2023-09-23 22:48:31 +0900

    input accept 속성추가

--- src/app/components/Blob.tsx
+++ src/app/components/Blob.tsx
@@ -5,6 +5,7 @@ const BlobImg = (): JSX.Element => {
   const [imgList, setImgList] = useState<string[]>();
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
+    console.log(file);
 
     if (file !== undefined) {
       const blobURL = URL.createObjectURL(file);
@@ -13,7 +14,7 @@ const BlobImg = (): JSX.Element => {
   };
   return (
     <>
-      <input type="file" onChange={onChange} />
+      <input type="file" accept="image/jpeg, image/png, image/webp" onChange={onChange} />
       <img src={imgList?.[0]} alt="123" />
     </>
   );
